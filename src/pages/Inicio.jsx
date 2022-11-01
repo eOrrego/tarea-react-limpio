c01e8c4c48158ee9395478e86fa9fa735ba6bd9e
SteveO <orregoe@gmail.com>
2022-11-01 00:41:24 -0300

    Boton peliculas polularas

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Inicio = () => {
   return (
     <div>
-        <h1>Página de Inicio</h1>
-        <p>Está es la página de inicio</p>
+      <h1>Página de Inicio</h1>
+      <p>Está es la página de inicio</p>
+
+      <Link to="/articulos">
+        <button className="btn btn-outline-info m-auto">Peliculas Polulares</button>
+      </Link>
+
     </div>
   )
 }
